3ba69487a59bd4debcf039039166ba58164dc335
Thiago Amâncio <127698239+MoonAmon@users.noreply.github.com>
2023-11-27 10:54:20 -0300

    first commit

--- AppNotifica/Source/Coordinator/LoginCoordinator.swift
+++ AppNotifica/Source/Coordinator/LoginCoordinator.swift
@@ -16,21 +16,22 @@ class LoginCoordinator: Coordinator {
     //cria um construtor para incializar meu navationCrontroller
     init (navigationController: UINavigationController ) {
             self.navigationController = navigationController
-     
         }
 
       func start() {
-          //chama a login
-        let viewController = LoginViewController()
-            self.navigationController.pushViewController(viewController, animated: true)
-
-          viewController.onRegiterTap = {
+     //chama a login
+     let viewController = LoginViewController()
+          self.navigationController.pushViewController(viewController, animated: true)
+          
+          
+          viewController.onRegisterTap = {
               self.gotoRegister()
           }
     }
     
-    func gotoRegister() {
-        let coordinator = RegisterCoordinator(navigationController: navigationController)
+    //função que chama a registerView
+    func  gotoRegister() {
+       let coordinator = RegisterCoordinator(navigationController: navigationController)
         coordinator.start()
     }
 }
